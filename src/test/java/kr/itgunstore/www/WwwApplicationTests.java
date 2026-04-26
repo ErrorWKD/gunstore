11f86e9fdc8b3adfde05d6b1d12b57decb7067c9
ErrorWKD <killingfillyou@gmail.com>
2026-04-26 13:35:58 +0900

    gun project 생성

---
+++ src/test/java/kr/itgunstore/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.itgunstore.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
